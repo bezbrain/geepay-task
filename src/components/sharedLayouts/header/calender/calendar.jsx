236d4f1ad70f960c064381c5f85c7a664fd34e5d
bezbrain <belloquadriolawale@gmail.com>
2024-01-23 20:18:43 +0100

    Style the calendar

--- src/components/sharedLayouts/header/calender/calendar.jsx
+++ src/components/sharedLayouts/header/calender/calendar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { solarCalendar } from "../../../../assets/icons/headersIcons";
+import styles from "./calendar.module.css";
 
 const Calendar = () => {
   return (
-    <div>
+    <div className={styles.calendar}>
       <img src={solarCalendar} alt="Calendar" />
       <p>November 15, 2023</p>
     </div>
