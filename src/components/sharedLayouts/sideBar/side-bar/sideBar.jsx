03866d101ab8cff572db549af390b6e649beb05f
bezbrain <belloquadriolawale@gmail.com>
2024-01-24 09:31:26 +0100

    Write the mobile development for side bar

--- src/components/sharedLayouts/sideBar/side-bar/sideBar.jsx
+++ src/components/sharedLayouts/sideBar/side-bar/sideBar.jsx
@@ -5,24 +5,26 @@ import { frame, moon } from "../../../../assets/icons/sideBarIcons";
 
 const SideBar = () => {
   return (
-    <section className={styles.side__bar}>
-      <div className={styles.top__icons}>
-        {topIcons.map((each) => {
-          const { id, icon, name } = each;
-          return <img src={icon} alt={name} key={id} />;
-        })}
+    <section className={styles.sidebar__section}>
+      <div className={styles.side__bar}>
+        <div className={styles.top__icons}>
+          {topIcons.map((each) => {
+            const { id, icon, name } = each;
+            return <img src={icon} alt={name} key={id} />;
+          })}
 
-        <div className={styles.light__dark}>
-          <img src={frame} alt="frame" />
-          <img src={moon} alt="moon" />
+          <div className={styles.light__dark}>
+            <img src={frame} alt="frame" />
+            <img src={moon} alt="moon" />
+          </div>
         </div>
-      </div>
 
-      <div className={styles.base__icons}>
-        {baseIcon.map((each) => {
-          const { id, icon, name } = each;
-          return <img src={icon} alt={name} key={id} />;
-        })}
+        <div className={styles.base__icons}>
+          {baseIcon.map((each) => {
+            const { id, icon, name } = each;
+            return <img src={icon} alt={name} key={id} />;
+          })}
+        </div>
       </div>
     </section>
   );
