e0810862d38b7cc8bdd7ca98726aba180e1219ba
bezbrain <belloquadriolawale@gmail.com>
2024-01-26 20:16:03 +0100

    Handle the profile at tablet screen

--- src/components/sharedLayouts/header/profile/profile.jsx
+++ src/components/sharedLayouts/header/profile/profile.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { profileDetails } from "../../../../assets/images/headerImages";
 import styles from "./profile.module.css";
 import { FaAngleDown } from "react-icons/fa6";
 
 const Profile = ({ isDark }) => {
+  const [isProfile, setIsProfile] = useState(false);
+
   return (
-    <div className={styles.profile__details}>
+    <div
+      className={styles.profile__details}
+      onClick={() => setIsProfile(!isProfile)}
+    >
       <img src={profileDetails} alt="Profile" />
       <div className={styles.profile__name}>
         <h4 className={`${isDark ? styles.dark__name : styles.light__name}`}>
@@ -21,6 +26,17 @@ const Profile = ({ isDark }) => {
           isDark ? styles.calendar__dark : styles.calendar__light
         } ${styles.profile__icon}`}
       />
+
+      {isProfile && (
+        <div className={styles.toggle__profile__name}>
+          <h4 className={`${isDark ? styles.dark__name : styles.light__name}`}>
+            Justin Bergson
+          </h4>
+          <p className={`${isDark ? styles.dark__name : styles.light__name}`}>
+            Justin@gmail.com
+          </p>
+        </div>
+      )}
     </div>
   );
 };
