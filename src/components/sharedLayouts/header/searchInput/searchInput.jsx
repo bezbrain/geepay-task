2c6932ec2820c96bd494488fe1c20044082b6830
bezbrain <belloquadriolawale@gmail.com>
2024-01-26 20:49:43 +0100

    Make amendment to the input icon at mobile screen

--- src/components/sharedLayouts/header/searchInput/searchInput.jsx
+++ src/components/sharedLayouts/header/searchInput/searchInput.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { iconSearch } from "../../../../assets/icons/headersIcons";
 import styles from "./search.module.css";
 
 const SearchInput = ({ isDark }) => {
+  const [isSearch, setIsSearch] = useState(true);
+
   return (
-    <div className={styles.search__input}>
-      <input
-        type="text"
-        placeholder="Search..."
-        className={`${isDark ? styles.input__dark : styles.input__light}`}
-      />
-      <img src={iconSearch} alt="Search" />
-    </div>
+    <>
+      <div className={styles.search__input}>
+        <input
+          type="text"
+          placeholder="Search..."
+          className={`${isDark ? styles.input__dark : styles.input__light}`}
+        />
+        <img
+          src={iconSearch}
+          alt="Search"
+          onClick={() => setIsSearch(setIsSearch(!isSearch))}
+        />
+      </div>
+      <div className={styles.toggle__input}>
+        {isSearch && <input type="text" placeholder="Search..." />}
+      </div>
+    </>
   );
 };
 
